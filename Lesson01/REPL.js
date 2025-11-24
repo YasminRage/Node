cc8eb15e908778ffc62c20636d1b326e6ad64cee
YasminRage <yasmin_rage@outlook.com>
2025-11-24 20:27:36 +0000

    Add files via upload

---
+++ Lesson01/REPL.js
@@ -0,0 +1,12 @@
+//REPL Command
+
+//.break (or .clear) - Exits block within the current REPL session.
+//.editor -  enters editor mode, allowing for multi-line input 
+//.exit - Exits the REPL session
+//.help - Displays help information for REPL commands
+//.load <filename> - Loads a file into the REPL session
+//.save <filename> - Saves the current REPL session to a file
+
+
+//Strict Mode
+// 'use strict'; - Enforces strict mode, which helps catch common coding errors that node ignores
